f5e7ff2bd30f947453a13fd240c7e9651434ca78
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-08 18:18:07 -0300

    Criando Area geral do APP

--- photo_gallery/src/App.tsx
+++ photo_gallery/src/App.tsx
@@ -2,7 +2,15 @@ import * as C from './App.styles';
 
 const App = () => {
   return(
-    <C.Container>...</C.Container>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/* Área de Uploads */}
+
+        {/* Lista de Fotos */}
+      </C.Area>
+    </C.Container>
   );
 };
 export default App;
