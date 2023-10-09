03661e272aa1cdd4b3ffe51c0591512625d3ba36
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-09 16:31:04 -0300

    criando funçao que faz o Ipload das fotos

--- photo_gallery/src/services/photos.ts
+++ photo_gallery/src/services/photos.ts
@@ -1,6 +1,7 @@
 import { Photo } from "../types/photo";
 import { storage } from "../libs/firebase";
-import { ref, listAll, getDownloadURL } from 'firebase/storage'
+import { ref, listAll, getDownloadURL, uploadBytes } from 'firebase/storage'
+import { v4 as createId } from 'uuid';
 
 
 export const getAll = async () => {
@@ -12,11 +13,24 @@ export const getAll = async () => {
     for(let i in photoList.items) {
         let photoUrl = await getDownloadURL(photoList.items[i]);
 
-        list.push({
-            name: photoList.items[i].name,
-            url: photoUrl
-        })
-    }
+        list.push({ name: photoList.items[i].name, url: photoUrl })
+    };
 
     return list;
-}
+};
+
+export const insert = async (file: File) => {
+    if([ 'image/jpeg', 'image/jpg', 'image/png' ].includes(file.type)) {
+
+        let randomName = createId();
+        let newFile = ref(storage, `image/${randomName}`);
+
+        let upload = await uploadBytes(newFile, file);
+        let photoUrl = await getDownloadURL(upload.ref);
+        
+        return { name: upload.ref.name, url: photoUrl } as Photo;
+
+    } else {
+        return new Error( 'Tipo de arquivo não permitido.' )
+    }
+};
